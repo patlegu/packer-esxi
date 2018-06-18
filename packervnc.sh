81ac583562e848a1c7db4f9d3a7a26670c3481db
patlegu <3754859+patlegu@users.noreply.github.com>
2018-06-18 07:49:17 +0000

    Update packervnc.sh
    Ajout des commandes à passer pour éditer le fichiers des régles du firewall de l'ESX.
    Modification de l'adresse de l'ESXi.

--- packervnc.sh
+++ packervnc.sh
@@ -1,2 +1,14 @@
-scp packervnc.xml esx:/etc/vmware/firewall/
-ssh esx 'esxcli network firewall refresh && esxcli network firewall ruleset list | grep packer-vnc'
+
+# Modifier le ficher /etc/vmware/firewall/service
+# Lancer les commandes suivantes pour pouvoir éditer le fichier.
+#   chmod 644 /etc/vmware/firewall/service
+#   chmod +t /etc/vmware/firewall/service.
+# Ajouter les lignes contenu dans le fichier packervnc.XML
+# Repositionner les droits sur le fichiers :
+#   chmod 444 /etc/vmware/firewall/services
+# Lancer les commandes suivantes :
+#   esxcli network firewall refresh
+#   esxcli network firewall ruleset list
+
+scp packervnc.xml <ESX ADDRESS>:/etc/vmware/firewall/
+ssh <ESX ADDRESS> 'esxcli network firewall refresh && esxcli network firewall ruleset list | grep packer-vnc'
